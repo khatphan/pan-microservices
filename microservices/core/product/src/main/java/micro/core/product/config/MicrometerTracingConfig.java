300a7af6054e8ec6116a73764d1ae63fb1950b4b
ADMIN-PHAN <phankhakhat@gmail.com>
2024-12-05 15:32:03 +0700

    [pan-microservices][zipkin] - add io.micrometer dependency

---
+++ microservices/core/product/src/main/java/micro/core/product/config/MicrometerTracingConfig.java
@@ -0,0 +1,24 @@
+package micro.core.product.config;
+
+import brave.handler.SpanHandler;
+import brave.Tracing;
+import brave.propagation.ThreadLocalCurrentTraceContext;
+import io.micrometer.tracing.Tracer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MicrometerTracingConfig {
+
+    /*@Bean
+    public Tracer tracer() {
+        Sender sender = URLConnectionSender.create("http://localhost:9411/api/v2/spans");
+        Reporter<zipkin2.Span> reporter = AsyncReporter.create(sender);
+        Tracing tracing = Tracing.newBuilder()
+                                 .currentTraceContext(ThreadLocalCurrentTraceContext.newBuilder().build())
+                                 .spanReporter(reporter)
+                                 .build();
+        return BraveTracer.create(tracing);
+    }*/
+
+}
